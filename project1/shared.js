f6de8aa57e71bb13e9a663bb4d293639af6d0e0c
Juan Luis Jimenez <jlja2@ensign.edu>
2023-09-13 14:42:54 -0600

    shared.js script to interact with the modal and the dropback, done

---
+++ project1/shared.js
@@ -0,0 +1,27 @@
+// js to manipulate styles
+
+
+//it will select the first element with .backdrop class
+let backdrop = document.querySelector('.backdrop');
+let selectPlanButton = document.querySelectorAll('.plan button');
+let modal = document.querySelector('.modal');
+let modalNoResponseButton = document.querySelector('.modal__action--negative');
+//console.dir(backdrop) // this will print the directory of the element with a bunch of properties that we can use
+//console.dir(selectPlanButton);
+//backdrop.style.display = 'block';
+
+for(let i=0; i < selectPlanButton.length; i++){
+    selectPlanButton[i].addEventListener('click', ()=>{
+        modal.style.display ='block';
+        backdrop.style.display = 'block';
+
+    });
+}
+
+backdrop.addEventListener('click', closeModal);
+modalNoResponseButton.addEventListener('click',closeModal);
+
+function closeModal(){
+    backdrop.style.display='none';
+    modal.style.display = 'none';
+}
