37a73d5af0c5778db5b94d8fdee8ff7dcf376992
Juan Luis Jimenez <jlja2@ensign.edu>
2023-09-14 00:37:25 -0600

    added a hamburger menu, js to change its style

--- project1/shared.js
+++ project1/shared.js
@@ -6,6 +6,8 @@ let backdrop = document.querySelector('.backdrop');
 let selectPlanButton = document.querySelectorAll('.plan button');
 let modal = document.querySelector('.modal');
 let modalNoResponseButton = document.querySelector('.modal__action--negative');
+let toggleButton = document.querySelector('.toggle-button');
+let mobileNav = document.querySelector('.mobile-nav');
 //console.dir(backdrop) // this will print the directory of the element with a bunch of properties that we can use
 //console.dir(selectPlanButton);
 //backdrop.style.display = 'block';
@@ -18,10 +20,19 @@ for(let i=0; i < selectPlanButton.length; i++){
     });
 }
 
-backdrop.addEventListener('click', closeModal);
+backdrop.addEventListener('click', ()=>{
+    mobileNav.style.display = 'none';
+    closeModal();
+});
 modalNoResponseButton.addEventListener('click',closeModal);
 
 function closeModal(){
     backdrop.style.display='none';
     modal.style.display = 'none';
 }
+
+toggleButton.addEventListener('click',()=>{
+    mobileNav.style.display="block";
+    backdrop.style.display ='block';
+})
+
